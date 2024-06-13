8545e7cd4ebe5c9248380f087ebb278a26d5121c
claimgame-bit <21102136.nausin.khan@gmail.com>
2024-06-13 16:41:33 +0530

    Add files via upload

---
+++ ecommerce web application/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;SELL ME AGAIN | All Rights Reserved | Contact Us: +91 9082466845</b></span></div>
+</footer>
+    
